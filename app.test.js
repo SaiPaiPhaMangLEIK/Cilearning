1570607860b025681d31182af7ecbb62c340cb96
Sai Leik <Leiksai742@gmail.com>
2026-07-09 14:15:54 +1000

    fixing test

--- app.test.js
+++ app.test.js
@@ -3,11 +3,11 @@ const { describe, it, expect } = require('@jest/globals');
 const app = require('./app');
 
 describe('GET /', () => {
-  it(`should return 'Works on my machine.'`, async () => {
+  it(`should return 'Hello World'`, async () => {
     const res = await request(app)
       .get('/')
       .expect(200);
 
-    expect(res.text).toBe('Works on my machine.');
+    expect(res.text).toBe('Hello World');
   });
 });
